db5085d602507d49adbd72c5210509cc0ffb9ae9
shuyuheng <shuyuheng@aliyun.com>
2021-07-02 15:23:25 +0800

    '7-2'

--- src/App.vue
+++ src/App.vue
@@ -8,31 +8,19 @@
 export default {
   created() {},
   mounted() {
-    // window.addEventListener("resize", this.monitor);
+    this.monitor();
   },
   methods: {
-    // 监听宽度适配对应显示页面
+    // 移动端不能编辑
     monitor() {
-      if (["/Exhibition", "/Adaptive"].includes(this.$route.path)) {
-        let pageWidth = document.documentElement.offsetWidth;
-        let pageHeight = document.documentElement.offsetHeight;
-        if (pageWidth > 750 && pageHeight > 600) {
-          if (this.$route.path != "/Exhibition")
-            this.$router.replace("/Exhibition");
-        } else {
-          if (this.$route.path != "/Adaptive")
-            this.$router.replace("/Adaptive");
-        }
+      let pageWidth = document.documentElement.offsetWidth;
+      let pageHeight = document.documentElement.offsetHeight;
+      if (pageWidth > 750 && pageHeight > 600) {
+      } else {
+        if (this.$route.path != "/Exhibition")
+          this.$router.replace("/Exhibition");
       }
     },
-  },
-  watch: {
-    "$route.path"() {
-      // this.monitor();
-    },
-  },
-  destroyed() {
-    window.removeEventListener("resize", this.monitor);
   },
 };
 </script>
